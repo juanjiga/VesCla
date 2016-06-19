ae214d9eefa1586f41975a7c9ddde49b53b01a30
juanjiga <juanjiga71@hotmail.es>
2016-06-19 23:02:56 +0200

    segundo commit

--- app/src/main/java/com/juanjiga/vescla/Clave.java
+++ app/src/main/java/com/juanjiga/vescla/Clave.java
@@ -3,7 +3,12 @@ package com.juanjiga.vescla;
 /**
  * Created by Luci on 19/06/2016.
  */
+
+// clase "pojo"
+
 public class Clave {
     private int id;
     private String nombre;
+    private String usuario;
+    private String password;
 }
