edaca77db5511b73a8766dc4c8c9c5f73ce71eca
juanjiga <juanjiga71@hotmail.es>
2016-06-26 12:17:00 +0200

    Añadida la Clase DBAdpater

--- app/src/main/java/com/juanjiga/vescla/Clave.java
+++ app/src/main/java/com/juanjiga/vescla/Clave.java
@@ -14,5 +14,29 @@ public class Clave {
         this.usuario = usuario;
         this.password = password;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
 
--- app/src/main/java/com/juanjiga/vescla/Constantes.java
+++ app/src/main/java/com/juanjiga/vescla/Constantes.java
@@ -3,10 +3,10 @@ package com.juanjiga.vescla;
 public class Constantes {
 
     //Nombre de la base de datos y versión
-    public static final String DB_NAME = "database.db";
+    public static final String DB_NAME = "base-dat.db";
     public static final int DB_VERSION = 1;
 
-    //Tabla Claves y campos
+    //Nombre de la tabla, "claves" y nombres de los campos (columnas)
     public static final String TABLA_CLAVES = "claves";
 
     public static final String ID = "id";
@@ -14,6 +14,7 @@ public class Constantes {
     public static final String USUARIO = "usuario";
     public static final String PASSWORD = "password";
 
+    //Sentencia SQL para la creación de la tabla
     public static final String TABLA_CLIENTES_SQL =
             "CREATE TABLE  " + TABLA_CLAVES + "(" +
                     ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
