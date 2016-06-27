ac9a3e05fceed3276550b7339daf3572fa416539
juanjiga <juanjiga71@hotmail.es>
2016-06-27 12:36:08 +0200

    Empezando DataBaseClaves

--- app/src/main/java/com/juanjiga/vescla/DataBaseClaves.java
+++ app/src/main/java/com/juanjiga/vescla/DataBaseClaves.java
@@ -2,11 +2,32 @@ package com.juanjiga.vescla;
 
 // basado en Codigo Alonso
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataBaseClaves {
 
     private DataBaseHelper dataBaseHelper;
     private SQLiteDatabase db;
+
+
+
+    //clase interna Helper
+    private static class DataBaseHelper extends SQLiteOpenHelper {
+        DataBaseHelper(Context context) {
+            super(context, DB_NAME, null, DB_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLA_CLAVES);
+            onCreate(db);
+        }
 
 }
