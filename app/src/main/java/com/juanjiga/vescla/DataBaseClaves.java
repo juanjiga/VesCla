e3edb898c077671b6d8f7ad4b8b29e00f4ed0b83
Juan <juanjiga@users.noreply.github.com>
2016-07-04 23:02:13 +0200

    desde casa

--- app/src/main/java/com/juanjiga/vescla/DataBaseClaves.java
+++ app/src/main/java/com/juanjiga/vescla/DataBaseClaves.java
@@ -11,18 +11,6 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataBaseClaves {
 
-    //Nombre de la base de datos y versión
-    public static final String DB_NAME = "vescladb.db";
-    public static final int DB_VERSION = 1;
-
-    //Nombre de la tabla, "claves" y nombres de los campos (columnas)
-    public static final String TABLA_CLAVES = "claves";
-
-    public static final String ID = "id";
-    public static final String NOMBRE = "nombre";
-    //public static final String USUARIO = "usuario";
-    public static final String PASSWORD = "password";
-
     private DataBaseHelper dataBaseHelper;
     private SQLiteDatabase db;
 
@@ -39,7 +27,7 @@ public class DataBaseClaves {
         if (db != null)
             db.close();
     }
-    //metodo para no repetir Values
+    //metodo para no repetir Values  ---->>>>>>> valoresssss
     private ContentValues clavesMapper(Clave clave) {
         ContentValues contentValues = new ContentValues();
         // (sin usar la clase "Constantes"   contentValues.put(USUARIO, clave.getUsuario());
