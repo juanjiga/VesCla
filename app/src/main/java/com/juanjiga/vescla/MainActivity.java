17d4f838c5343419ee44ea87e6298729faed17b9
juanjiga <juanjiga71@hotmail.es>
2016-07-06 13:36:32 +0200

    empezando en MainActivity

--- app/src/main/java/com/juanjiga/vescla/MainActivity.java
+++ app/src/main/java/com/juanjiga/vescla/MainActivity.java
@@ -1,20 +1,36 @@
 package com.juanjiga.vescla;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DataBaseControl database;
+    Cursor cursor;
+    ListView lista;
+    SimpleCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = new DataBaseControl(this);
+        lista = (ListView) findViewById(R.id.lista_listView);
+
+        String[] from = new String[]{database.}
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
